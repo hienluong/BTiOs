d53e37707ad7ab9c92063af022a9586b17e1071b
hienluong <luonghien286@gmail.com>
2013-04-04 15:16:30 +0700

    BT buoi 10
    abc

---
+++ DemoFish 3/DemoFish/Hook.h
@@ -0,0 +1,18 @@
+//
+//  Hook.h
+//  DemoFish
+//
+//  Created by Fighting on 4/4/13.
+//  Copyright (c) 2013 Techmaster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hook : NSObject
+{
+}
+@property (nonatomic,strong)  UIImage *imageHook;
+@property (nonatomic,strong)  UIImage *imageNewHook;
+@property (nonatomic,strong)  UIImageView *hookView;
+-(void) demoHook;
+@end
